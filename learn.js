6b1ef3ecd6974ae77fb983c7903fe567b79c9b0e
karan <karanbisht117@gmail.com>
2022-09-26 17:32:00 +0530

    intial commit

--- learn.js
+++ learn.js
@@ -1,6 +1,6 @@
 const sum = (x,y)=>{
     return x+y
 }
-console.log(x+y)
+console.log("sum of x+y is=",x+y)
 sum (10,12)
 console.log()
---
+++ multi.js
@@ -0,0 +1,5 @@
+const multi = (x,y)=>{
+return x*y
+}
+console.log(x*y)
+multi (12,23)
